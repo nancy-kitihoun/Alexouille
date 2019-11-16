d20a13ea28b451b09ba9f722cd5850c630637b44
nancy-kitihoun <nancy.kitihoun@yahoo.com>
2019-11-16 15:57:01 +0100

    please

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+
 library(shiny)
 library(shinydashboard)
 library(DT)
@@ -14,31 +15,35 @@ library(caret)
 library(parallelSVM)
 library(gbm)
 library(ggvis)
+library(ROCR)
+library(PRROC)
+library(pROC)
 
 
 
 options(shiny.maxRequestSize=150*1024^2)
 
 
-undersampling = function(data, p, position)
+
+under = function(data, pro)
 {
-    summary = table(data[,position])
+    Table = table(data[,31])
     
-    m_event_value = names(summary)[which.max(table(data[,position]))]
+    nombr_cible = names(Table)[which.max(table(data[,31]))]
     
-    m_event_index = which(data[,position]==m_event_value)
+    pos_index = which(data[,31]==nombr_cible)
     
-    m_event_length = length(m_event_index)
+    length_nomb = length(pos_index )
     
-    data_length = nrow(data)
+    taille = nrow(data)
     
-    new_m_event_length = floor(data_length - (data_length-m_event_length)/p)
+    nouv = floor(taille - (taille-length_nomb)/pro)
     
-    new_index = sample(m_event_index,size=new_m_event_length)
+    pos_plus = sample(pos_index,size=nouv)
     
-    new_data = data[-new_index,]
+    nouvdata = data[-pos_plus,]
     
-    new_data
+    nouvdata
     
 }
 
@@ -76,6 +81,154 @@ shinyServer(function(input, output,session) {
                                    target = "column", selected = 4)
         )
     })
+    output$Des1 =DT::renderDataTable({ 
+        datatable(profiling_num(data()),extensions = 'FixedColumns',editable = 'cell',
+                  rownames = F, options = list(pageLength = 4, dom = 'tp',scrollX = TRUE,
+                                               fixedColumns = TRUE),
+                  selection = list(mode = "single",
+                                   target = "column", selected = 4) )})
+    
+    output$Des2 =DT::renderDataTable({ 
+        datatable(df_status(data()),extensions = 'FixedColumns',editable = 'cell',
+                  rownames = F, options = list(pageLength = 5, dom = 'tp',scrollX = TRUE,
+                                               fixedColumns = TRUE),
+                  selection = list(mode = "single",
+                                   target = "column", selected = 4) )})
+    
+    selectedData <- reactive({
+        data()[1:input$taille, c(input$variable1, input$variable2)]
+        
+    })
+    #grade de liaison entre les variables.
+    B1=eventReactive(input$graph, {selectedData()})
+    
+    output$plot2 <- renderPlot({
+        
+        #par(mar = c(5.1, 4.1, 0, 1))
+        plot(B1(),col="#70678a")
+        
+    })
+    
+    
+    #height = 400
+    # width = 600})
+    output$Grap2<- renderPlot({freq(data()[,31])})
+    output$tabCor <-renderPlot({ ggcorr(data()[,-31])})
+    
+    # pour une page qui affiche tout un graph complet 
+    
+    
+    
+    output$Grap3<- renderPlot({plot_num_density(data()[,c(-31)]) 
+        height = 600
+        width = 800 })
+    
+    #------------------------------------------------ partie analyse
+    
+    # svm
+    inde=reactive({set.seed(123456)
+        sample(1:nrow(data()),nrow(data())*0.7)     })
+    
+    
+    
+    train=reactive({
+        
+        x = data()[inde(),]
+        x$Class = as.factor(x$Class)
+        x
+    })
+    
+    
+    test=reactive({
+        x = data()[-inde(),]
+        x$Class = as.factor(x$Class)
+        x
+    })
+    
+    b=reactive({input$prop} )
+    
+    appren=reactive({
+        under(train(),b())
+    })
+    
+    
+    svm_result=reactive({  
+        
+        svm(Class ~., data = appren(),
+            kernel = "linear",
+            type = "C-classification")
+    })
+    A1=eventReactive(input$proportion, {svm_result()})
+    output$svm1=renderPrint(A1())
+    
+    
+    a=reactive({
+        predict(A1(),test()[,1:30])
+    })
+    #predicte()
+    output$performence =renderPrint({confusionMatrix(predict(A1(),newdata=test()),test()$Class,positive="1")
+    })
+    q= reactive({
+        p= roc.curve(a(),as.factor(test()$Class),curve = TRUE,max.compute = TRUE, 
+                     min.compute = TRUE, rand.compute = TRUE)
+        plot(p,max.plot = TRUE, min.plot = TRUE, rand.plot = TRUE, fill.area = TRUE)})
+    output$ROC=renderPlot({q()})
+    
+    #abre de classification
+    tre=reactive({
+        tree(Class~., appren())})
+    A2=eventReactive(input$proportion, {tre()})
+    
+    output$tree=renderPrint({ Sys.sleep(1);
+        summary(A2())})
+    
+    output$abre= renderPlot({plot(A2())
+        text(A2(), pretty=0)    })
+    
+    
+    
+    output$treroc=renderPrint({ Sys.sleep(1)
+        
+        tree.pred=predict(A2(),newdata=test(),type="class")
+        
+        confusionMatrix(as.factor(tree.pred),test()[,31],positive="1")
+        
+    })
+    
+    
+    
+    # gradient boosting
+    boost=reactive({
+        
+        g=gbm(as.character(Class)~., data=appren(), distribution="bernoulli", n.trees= 500, interaction=4)
+        g})
+    A3=eventReactive(input$proportion, {boost()})
+    output$gradient=renderPlot(summary(A3()))
+    
+    output$perfgradient=renderPrint({
+        
+        pred.boost=predict(A3(), newdata=test(), n.trees=500,type ='response',interaction=4) 
+        
+        class_pred = ifelse(pred.boost<0.5, "0", "1")   
+        confusionMatrix(as.factor(class_pred), test()$Class, positive = "1")
+    })
+    
+    #logistique 
+    logis=reactive({
+        glm.fit2=glm(Class~.,data=appren(),family=binomial)})
+    A4=eventReactive(input$proportion, {logis()})
+    output$logs=renderPrint({summary(A4())})
+    
+    
+    
+    #confus=reactive({
+        #glm.probs=predict(A4(),test(), type="response")
+        #glm.pred=rep(0,nrow(test()))
+        #glm.pred[glm.probs>.5]=1
+        #confusionMatrix(as.factor(glm.pred), test()$Class, positive = "1")
+    #})
+    #output$perflog=renderPrint({confusionMatrix(as.factor(confus()), test()$Class, positive = "1")})
+    
 })
 
 
--- ui.R
+++ ui.R
@@ -1,4 +1,7 @@
 
+library(PRROC)
+library(pROC)
+library(ROCR)
 library(shiny)
 library(shinydashboard)
 
@@ -109,10 +112,114 @@ dashboardPage(
                                 box(width = 12, status = "primary",
                                     title = "detection de fraude sur des cartes bancaires", 
                                     DT::dataTableOutput("dataTable")
-                                ))
-                     )
-              )
-      )
-    )
-  )
+                                ),
+                                box(status = "primary", 
+                                    title=(" simple statistique descriptive"),
+                                    DT::dataTableOutput("Des1")),
+                                
+                                box(status = "primary",tags$footer(tags$em(
+                                  "le nombre de valeurs nulles (q_zeros) et son pourcentage (p_zeros)",br(),
+                                  "le nombre de valeurs manquantes (q_na) et son pourcentage (p_na)",br(),
+                                  "le nombre de valeur infinies (q_inf) et son pourcentage (p_inf)",br())),
+                                  
+                                  DT::dataTableOutput("Des2")))),
+                     tabPanel("relation entre les variables",
+                              
+                              
+                              
+                              fluidRow(columns=12,
+                                       sidebarPanel(
+                                         
+                                         #Definition 1er menu deroulant
+                                         selectInput("variable1", "Variable (axe des x):",
+                                                     list("Time" , "V1" ,"V2", "V3" ,"V4" , "V5" ,"V6","V7" ,"V8" ,
+                                                          "V9","V10","V11" , "V12","V13","V14"  , 
+                                                          "V15", "V16"   , "V17"   , "V18" ,   "V19"  ,  "V20"  ,  "V21"  ,  
+                                                          "V22", "V23", "V24", "V25" , "V26", "V27"  ,  "V28" , "Amount" )),
+                                         
+                                         #Definition du second menu deroulant
+                                         selectInput("variable2", "Variable (axe des y):",
+                                                     list( "V1" ,"V2", "V3" ,"V4" , "V5" ,"V6","V7" ,"V8" ,
+                                                           "V9","V10","V11" , "V12","V13","V14"  , 
+                                                           "V15", "V16"   , "V17"   , "V18" ,   "V19"  ,  "V20"  ,  "V21"  ,  
+                                                           "V22", "V23", "V24", "V25" , "V26", "V27"  ,  "V28" , "Amount","Time")),
+                                         sliderInput("taille", "Choisir la taille des données" , min=1000, max=100000, value=500
+                                         ),
+                                         actionButton(inputId="graph", "Go!"),
+                                         p("Click the button to update the value displayed in the main panel.",br(),
+                                           "NB:Augmentation de la taile des données augmentera , le temps exécution ")),
+                                       
+                                       mainPanel( plotOutput("plot2")))),
+                     tabPanel("Stat.descriptives2",
+                              
+                              fluidPage(
+                                box( title="fréquence de la variables Classe",status = "primary",  plotOutput("Grap2", height = "300px"),tags$footer(tags$em("Proportion des 0 et 1  dans la variable dépendante"))),
+                                box( title="Corrélation", plotOutput("tabCor") ,status = "primary",tags$footer(tags$em("En probabilités et en statistique, 
+                              la corrélation entre plusieurs variables aléatoires ou statistiques est une notion de liaison qui contredit leur indépendance.
+                               Ici  nous avons des varaiables trés peu correler entre elles "))))
+                     ),
+                     tabPanel("Densité des variables", tatus= "primary",width = 24,
+                              plotOutput("Grap3", width = "100%" ),tags$footer(tags$em("Les variables sont toutes centrées sur zéro "))
+                              
+                     ))),
+      # Analyse
+      tabItem(tabName="analysis",
+              
+              sidebarLayout(
+                sidebarPanel(
+                  sliderInput("prop", "Choisir la propotion des 0 dan l'echantillon d'apprentissage" , min=0.01, max=0.5, value=0.05
+                  ),
+                  actionButton(inputId="proportion", "Go!"),
+                  p("Click the button to update the value displayed in the main panel.")),
+                
+                
+                mainPanel(
+                  tabBox(type = "pills",selected = "SVM",width = 12,
+                         
+                         tabPanel("SVM", 
+                                  fluidPage(
+                                    box(width = 6, verbatimTextOutput("svm1"),status = "primary", title="sortie svm"),
+                                    box(width=6,verbatimTextOutput("performence"),status = "primary",title="matrice de confusion")),
+                                  
+                                  
+                                  
+                                  
+                         ),
+                         tabPanel("Abre de classification",
+                                  fluidPage(
+                                    box(width = 6, verbatimTextOutput("tree"),status = "primary", title="sortie tree",tags$footer(tags$em("Misclassification error rate estt ici 
+                                                    le taux d'erreure d'apprentissage. Comme nous pouvons le voir est faible "))),
+                                    box(width = 6, plotOutput("abre", height = "300px" ),status = "primary"),
+                                    box(width = 6, verbatimTextOutput("treroc"),status = "primary")
+                                    
+                                    
+                                  )),
+                         tabPanel("gradient boosting",
+                                  fluidPage(
+                                    box(width = 6, plotOutput("gradient"),status = "primary", title="gradient boosting"),
+                                    box(width = 6, verbatimTextOutput("perfgradient" ),status = "primary"))
+                                  
+                         ),
+                         tabPanel("logistique",
+                                  fluidPage(
+                                    box(width = 6, verbatimTextOutput("logs"),status = "primary", title="logistique"),
+                                    #box(width = 6, verbatimTextOutput("perflog" ),status = "primary",title="performance")
+                                    
+                                    
+                                  ))
+                         
+                         
+                         
+                         
+                         
+                         
+                  ))))
+    ))
+  
+  
+  
 )
+                                
+                                
+                                
+                                
